c200df8b1c2e48535adcc916c5ed4786c32639b5
Александр Заболотских <zabolotskikh@elma-bs.ru>
2024-03-03 23:42:59 +0300

    Добавил converter

--- internal/api/chat/send_message.go
+++ internal/api/chat/send_message.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/a1exCross/chat-server/internal/model"
+	"github.com/a1exCross/chat-server/internal/api/chat/converter"
 	chatPb "github.com/a1exCross/chat-server/pkg/chat_v1"
 	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/protobuf/types/known/emptypb"
@@ -12,11 +12,7 @@ import (
 
 // SendMessage принимает и обрабатывает запрос на отправку сообщения в чате
 func (i *Implementation) SendMessage(ctx context.Context, req *chatPb.SendMessageRequest) (*empty.Empty, error) {
-	err := i.messageServ.SendMessage(ctx, model.MessageDTO{
-		Author:    req.Message.From,
-		Content:   req.Message.Text,
-		CreatedAt: req.Message.Timestamp.AsTime(),
-	})
+	err := i.messageServ.SendMessage(ctx, converter.ProtoToMessage(req.Message))
 	if err != nil {
 		return nil, fmt.Errorf("failed to send message: %v", err)
 	}
